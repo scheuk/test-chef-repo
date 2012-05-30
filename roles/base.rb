33ce87384366f770b0c7feb046e059bdb81932a2
scheuk <kevin@nanbox.net>
2012-05-30 12:45:57 -0500

    Added base role

---
+++ roles/base.rb
@@ -0,0 +1,8 @@
+# A Base role that every skillabs server should have
+
+name "base"
+description "Base Packages that all SkilLabs server should have"
+
+run_list "recipe[ubuntu]", "recipe[ntp]", "recipe[logrotate]", "recipe[openssh]", "recipe[chef-client::config]", "recipe[chef-client::service]"
+
+

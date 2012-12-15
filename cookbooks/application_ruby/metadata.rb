a9fbcd895ad37f27b02eb47e935458fd85eb17b0
Kevin Scheunemann <kevin@nanbox.net>
2012-12-15 11:02:12 -0600

    Import application_ruby version 1.0.6

--- cookbooks/application_ruby/metadata.rb
+++ cookbooks/application_ruby/metadata.rb
@@ -1,9 +1,10 @@
+name             "application_ruby"
 maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Ruby-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.6"
 
 %w{ application runit unicorn apache2 passenger_apache2 }.each do |cb|
   depends cb

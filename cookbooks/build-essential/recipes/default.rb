2765b777c33dbb4bdcb4be2dc144ba7cac71ee1f
Kevin Scheunemann <kevin@nanbox.net>
2013-02-05 21:35:31 -0600

    Import build-essential version 1.3.4

--- cookbooks/build-essential/recipes/default.rb
+++ cookbooks/build-essential/recipes/default.rb
@@ -21,8 +21,8 @@ require 'chef/shell_out'
 
 compiletime = node['build_essential']['compiletime']
 
-case node['platform']
-when "centos", "redhat", "suse", "fedora", "scientific", "amazon", "ubuntu","debian"
+case node['platform_family']
+when "rhel", "suse", "fedora", "debian"
 
   # on apt-based platforms when first provisioning we need to force
   # apt-get update at compiletime if we are going to try to install at compiletime
@@ -64,14 +64,15 @@ when "centos", "redhat", "suse", "fedora", "scientific", "amazon", "ubuntu","deb
 when "smartos"
     include_recipe 'pkgin'
     %w{gcc47 gcc47-runtime scmgit-base gmake pkg-config binutils}.each do |package|
-			pkgin_package package do 
-      	action :install 
-    	end
-		end
+      pkgin_package package do
+        action :install
+      end
+    end
 
-when "darwin"
+when "mac_os_x"
   result = Chef::ShellOut.new("pkgutil --pkgs").run_command
-  installed = result.stdout.split("\n").include?("com.apple.pkg.gcc4.2Leo")
+  osx_gcc_installer_installed = result.stdout.split("\n").include?("com.apple.pkg.gcc4.2Leo")
+  developer_tools_cli_installed = result.stdout.split("\n").include?("com.apple.pkg.DeveloperToolsCLI")
   pkg_filename = File.basename(node['build_essential']['osx']['gcc_installer_url'])
   pkg_path = "#{Chef::Config[:file_cache_path]}/#{pkg_filename}"
 
@@ -79,13 +80,13 @@ when "darwin"
     source node['build_essential']['osx']['gcc_installer_url']
     checksum node['build_essential']['osx']['gcc_installer_checksum']
     action ( compiletime ? :nothing : :create )
-    not_if { installed }
+    not_if { osx_gcc_installer_installed or developer_tools_cli_installed  }
   end
   r.run_action(:create) if compiletime
 
   r = execute "sudo installer -pkg \"#{pkg_path}\" -target /" do
     action ( compiletime ? :nothing : :run )
-    not_if { installed }
+    not_if { osx_gcc_installer_installed or developer_tools_cli_installed  }
   end
   r.run_action(:run) if compiletime
 end
